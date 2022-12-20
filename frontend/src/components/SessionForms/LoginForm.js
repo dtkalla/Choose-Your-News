ae847eca2bd18622abb6b36b64bab9fff6eca36f
IshsGit <aishtahad@yahoo.com>
2022-12-20 16:57:13 -0500

    navbar and login styling

--- frontend/src/components/SessionForms/LoginForm.js
+++ frontend/src/components/SessionForms/LoginForm.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 import './SessionForm.css';
 
-import { login, clearSessionErrors } from '../../store/session';
+import { login, logout, clearSessionErrors } from '../../store/session';
 
 function LoginForm () {
   const [email, setEmail] = useState('');
@@ -26,33 +27,72 @@ function LoginForm () {
     dispatch(login({ email, password })); 
   }
 
+  const loginDemo = () => {
+    setEmail('demo-user@appacademy.io');
+    setPassword('starwars')
+    // const demoUser = {
+
+    //   email: 'demo-user@appacademy.io',
+    //   password: "password"
+    // };
+    // console.log('email below')
+    // console.log(demoUser.password)
+    if(email && password)
+      dispatch(login(email,password)); 
+  };
+
+  const logout = () => {
+    dispatch(logout());
+  };
+
   return (
+    <>
+    <div className='title'>
+        <h1>Choose</h1>
+        <h1>Your</h1>
+        <h1>News</h1>
+      </div>
+
+    <div className='form-container'>
     <form className="session-form" onSubmit={handleSubmit}>
-      <h2>Log In Form</h2>
-      <div className="errors">{errors?.email}</div>
+
+     
       <label>
-        <span>Email</span>
+        <span className='field'>Email</span>
         <input type="text"
           value={email}
           onChange={update('email')}
           placeholder="Email"
         />
       </label>
-      <div className="errors">{errors?.password}</div>
+      <div className="errors">{errors?.email}</div>
+    
       <label>
-        <span>Password</span>
+        <span className='field'>Password</span>
         <input type="password"
           value={password}
           onChange={update('password')}
           placeholder="Password"
         />
       </label>
-      <input
+      <div className="errors">{errors?.password}</div>
+      <div className='buttons'>
+      <input className='signup-button'
         type="submit"
         value="Log In"
         disabled={!email || !password}
-      />
+      />        
+    <button className="signup-button" type="submit" onClick={loginDemo}>
+      Demo Login
+    </button>
+ 
+        </div>
+        <div className='signup-link'>
+        <Link  to='/signup'>Signup</Link>
+        </div>
     </form>
+    </div>
+    </>
   );
 }
 
