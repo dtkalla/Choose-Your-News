a4ca0309a760c2ac4c5d17355f034419eb137ee1
Daniel <danieltkalla@gmail.com>
2022-12-22 09:27:11 -0500

    choose most recent articles

--- backend/config/api.js
+++ backend/config/api.js
@@ -16,7 +16,7 @@ exports.fetchArticlesFromNewYorkTimes = async (query) => {
         }
     }
 
-    const url = `${newyorktimesUrl}${filterQueryString}api-key=${newyorktimesApiKey}`;
+    const url = `${newyorktimesUrl}sort=newest&${filterQueryString}&api-key=${newyorktimesApiKey}`;
     const response = await axios.get(url);
     const data = response.data.response.docs;
     const articles = data.map(datum => {
@@ -28,5 +28,5 @@ exports.fetchArticlesFromNewYorkTimes = async (query) => {
             url: datum.web_url
         }
     })
-    return articles;
+    return articles.sort((a, b) => (a.publishedDate < b.publishedDate) ? 1 : -1);
 }
