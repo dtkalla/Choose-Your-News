1f08c0397ed8a73cbc154fa4a849eccefd77020d
Qiao Yang Han <qyhwork@gmail.com>
2022-12-26 03:03:49 -0500

    Worked on FiguresSidebar.js

--- frontend/src/components/Figures/FiguresSidebar.js
+++ frontend/src/components/Figures/FiguresSidebar.js
@@ -1,37 +1,40 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { fetchCurrentUserFetchedArticles, fetchCurrentUserFetchedArticlesByGroup, fetchCurrentUserFetchedArticlesByFigure } from '../../store/articles';
 import FigureCreate from "./FigureCreate";
 import FiguresIndex from "./FiguresIndex";
 import GroupDelete from "../Groups/GroupDelete";
 import '../MainPage/MainPage.css';
 
-function FiguresSidebar({ selectedGroupId, setSelectedGroupId, setSelectedFigureId }) {
+function FiguresSidebar({ selectedGroupId, setSelectedGroupId, selectedFigureId, setSelectedFigureId }) {
   const dispatch = useDispatch();
 
   useEffect(() => {
-      if(selectedGroupId){
+    if (selectedFigureId) {
+      // dispatch(fetchCurrentUserFetchedArticlesByGroup(selectedGroupId));
+    }
+    else {
+      if (selectedGroupId) {
         dispatch(fetchCurrentUserFetchedArticlesByGroup(selectedGroupId));
       }
       else {
         dispatch(fetchCurrentUserFetchedArticles());
       }
+    }
   }, [selectedGroupId])
 
   return (
-    <>
-      <div className="index-sidebar-container">
-        <div className="index-sidebar-title">
-          <FigureCreate selectedGroupId={selectedGroupId} />
-        </div>
-        <div className="index-sidebar-groups-container">
-          <hr></hr>
-          <FiguresIndex selectedGroupId={selectedGroupId} setSelectedFigureId={setSelectedFigureId} />
-          <hr></hr>
-          <GroupDelete selectedGroupId={selectedGroupId} setSelectedGroupId={setSelectedGroupId} />
-        </div>
+    <div className="index-sidebar-container">
+      <div className="index-sidebar-title">
+        <FigureCreate selectedGroupId={selectedGroupId} />
       </div>
-    </>
+      <div className="index-sidebar-groups-container">
+        <hr></hr>
+        <FiguresIndex selectedGroupId={selectedGroupId} setSelectedFigureId={setSelectedFigureId} />
+        <hr></hr>
+        <GroupDelete selectedGroupId={selectedGroupId} setSelectedGroupId={setSelectedGroupId} />
+      </div>
+    </div>
   );
 }
 
--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -5,6 +5,7 @@ import { fetchCurrentUserGroups } from '../../store/groups';
 import { fetchCurrentUserSavedArticles } from '../../store/articles';
 
 import NewsIndex from './NewsIndex';
+
 import GroupsIndex from '../Groups/GroupsIndex';
 import FiguresSidebar from '../Figures/FiguresSidebar';
 
@@ -14,47 +15,24 @@ import './MainPage.css';
 function MainPage() {
   const currentUser = useSelector(state => state.session.user);
 
-  // const groupsObj = useSelector(state => state.groups);
+  // const fetchedArticlesObj = useSelector(state => state.articles.fetched);
 
-  const fetchedArticlesObj = useSelector(state => state.articles.fetched);
+  // const savedArticlesObj = useSelector(state => state.articles.saved);
 
-  const savedArticlesObj = useSelector(state => state.articles.saved);
+  // const fetchedArticles = fetchedArticlesObj ? Object.values(fetchedArticlesObj) :[];
 
-  // const groups = groupsObj ? Object.values(groupsObj) : [];
-
-  const fetchedArticles = fetchedArticlesObj ? Object.values(fetchedArticlesObj) :[];
-
-  const savedArticles = savedArticlesObj ? Object.values(savedArticlesObj) : [];
-
-
-  function getFigures(groups) {
-    const figures = [];
-    if (groups) {
-      for (let i = 0; i < groups.length; i++) {
-        for (let j = 0; j < groups[i].figures.length; j++){
-          figures.push(groups[i].figures[j]);
-        }
-      }
-    }
-    return figures;
-  }
-
-  // const figures = groups ? getFigures(groups) : [];
+  // const savedArticles = savedArticlesObj ? Object.values(savedArticlesObj) : [];
 
   const [selectedGroupId, setSelectedGroupId] = useState(undefined);
 
   const [selectedFigureId, setSelectedFigureId] = useState(undefined);
 
-
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (currentUser) {
-      // dispatch(fetchCurrentUserGroups());
+      dispatch(fetchCurrentUserGroups());
       // dispatch(fetchCurrentUserSavedArticles());
-    }
-  }, [dispatch, currentUser])
-
+  }, [dispatch])
 
   return (
     <>
@@ -68,14 +46,10 @@ function MainPage() {
           savedArticles={savedArticles}
           selectedFigureId={selectedFigureId}
         /> */}
-
         <FiguresSidebar
           selectedGroupId={selectedGroupId}
           setSelectedGroupId={setSelectedGroupId}
           setSelectedFigureId={setSelectedFigureId}
-          // figures={
-          //   selectedGroupId === undefined ? figures : groupsObj[selectedGroupId].figures
-          // }
         />
       </div>
     </>
