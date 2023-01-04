c817f3082555962cf29a9380fce64941d25f30d6
Daniel <danieltkalla@gmail.com>
2023-01-04 11:04:51 -0500

    reword instructions modal

--- frontend/src/components/NavBar/Info.js
+++ frontend/src/components/NavBar/Info.js
@@ -26,18 +26,12 @@ function Info() {
                     <h1 className="instructions-header">Instructions</h1>
                     <h1 className="instructions-title">Groups</h1>
                     <p className="instructions-details">
-                        Beginning with a default group, All, which contains all the figures you follow,
-                        you may create a new group by clicking the Create Group button. In the newly 
-                        created group, you may add a figure that doesn't already belong to another group.
-                        One figure cannot belong in two different groups at the same time. You may remove
-                        the figure from a group if you wish to add it to a different group or delete the
-                        figure entirely.
+                        The default group, All, contains all figures you follow -- you can create more specific groups as well. In the newly created group, you can add a figure that doesn't already belong to another group.
+                        One figure cannot belong to two different groups at the same time. You can remove the figure from a group if you wish to add it to a different group, leave it ungrouped, or delete the figure entirely.
                     </p>
                     <h1 className="instructions-title">Figures</h1>
                     <p className="instructions-details">
-                        You can create a figure in your default, All, group. By clicking on a figure on
-                        the sidebar, you can fetch articles related only to that figure. You may add
-                        ungrouped figures from your default All group into your custom groups.
+                        When you create a figure, it starts as just part of the all group. By clicking on a figure on the sidebar, you can fetch articles related only to that figure. You can add ungrouped figures from your default All group into your custom groups.
                     </p>
                 </div>
             </LargeModal>
